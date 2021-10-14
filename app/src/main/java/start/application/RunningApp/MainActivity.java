d584f934e454f2b472ec7da1584a7df6f49fab8d
BrandenFoy98 <bf3787@uncw.edu>
2021-10-14 16:10:25 -0400

    renamed application

--- app/src/main/java/start/application/RunningApp/MainActivity.java
+++ app/src/main/java/start/application/RunningApp/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.myapplication;
+package start.application.RunningApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.application.RunningApp.R;
 
 public class MainActivity extends AppCompatActivity {
 
