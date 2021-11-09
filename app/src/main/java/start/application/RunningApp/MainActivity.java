37dc28a6427ead64af766c87ef0a19054b16570a
BrandenFoy98 <bf3787@uncw.edu>
2021-11-09 00:02:27 -0500

    Image added to homescreen

--- app/src/main/java/start/application/RunningApp/MainActivity.java
+++ app/src/main/java/start/application/RunningApp/MainActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,9 +17,17 @@ import com.application.RunningApp.R;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView rImage;
 
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth auth;
@@ -26,6 +37,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rImage = findViewById(R.id.rImage);
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        DatabaseReference getImage = databaseReference.child("image");
+
+        getImage.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // getting a DataSnapshot for the location at the specified
+                // relative path and getting in the link variable
+                String link = dataSnapshot.getValue(String.class);
+
+                // loading that data into rImage
+                // variable which is ImageView
+                Picasso.get().load(link).into(rImage);
+            }
+
+            // this will called when any problem
+            // occurs in getting data
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // we are showing that error message in toast
+                Toast.makeText(MainActivity.this, "Error Loading Image", Toast.LENGTH_SHORT).show();
+            }
+        });
         //get firebase auth instance
         auth = FirebaseAuth.getInstance();
 
